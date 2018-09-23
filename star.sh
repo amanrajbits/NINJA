6aa6d72bf204869c9c5976bd707952c6b8ff2a90
amanrajbits <amanraj.bits.@gmail.com>
2018-09-23 16:59:27 +0800

    mesg for jenkins auto build

--- star.sh
+++ star.sh
@@ -4,4 +4,4 @@ w=${1-5}
 line() { printf "%$(($1+w))s\n" "$(yes "* " | sed ${1}q | tr -d \\n)"; }
 for i in $(seq $w) $(seq $((w-1)) -1 1); do line $i; done
 
-# Jenkins build on auto commit 
+# Jenkins build trigger on auto commit 
